d0f86de938b2286c3f0667fc6cc75454492ef926
Joel <joelcox.joel@gmail.com>
2023-08-03 21:30:51 +0100

    Updates

--- precache-manifest.60631eca0b04e336463f67eeb036d8ed.js
+++ precache-manifest.60631eca0b04e336463f67eeb036d8ed.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "f7357ee80e7a72f44241",
+    "revision": "e1fd23e46518190197a7",
     "url": "/static/css/main.4c32aae7.chunk.css"
   },
   {
-    "revision": "f7357ee80e7a72f44241",
-    "url": "/static/js/main.aca1321e.chunk.js"
+    "revision": "e1fd23e46518190197a7",
+    "url": "/static/js/main.3079fd08.chunk.js"
   },
   {
     "revision": "0895a1e67ad5037d4bf0",
     "url": "/static/js/runtime~main.4d78919c.js"
   },
   {
-    "revision": "7f7a2f78ed830e87216c",
-    "url": "/static/js/2.9114d068.chunk.js"
+    "revision": "e2d57f37747194f02c8f",
+    "url": "/static/js/2.2c0dc3e3.chunk.js"
   },
   {
     "revision": "086eeff109b4c5754a35",
     "url": "/static/js/3.91a368b8.chunk.js"
   },
   {
-    "revision": "f27fb18484e5ade63ad812a1607462f9",
+    "revision": "2d9636cac7300ae341fb1673d315cf83",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d572938055ba0677e4e7fe6fccba3f6.js"
+  "/precache-manifest.60631eca0b04e336463f67eeb036d8ed.js"
 );
 
 workbox.clientsClaim();

35bdc9191c0e2435b0b3bd1b90ed4f294da90d44
Joel <joelcox.joel@gmail.com>
2023-09-29 19:22:41 +0100

    new rules

--- website/src/Components/Btv/BtvRules.tsx
+++ website/src/Components/Btv/BtvRules.tsx
@@ -11,7 +11,6 @@ import {
   Viewer,
   Worker,
 } from "@react-pdf-viewer/core";
-import { ScrollMode } from "@react-pdf-viewer/core";
 import { Box } from "@mui/material";
 
 export function BtvRulesPage() {
@@ -25,55 +24,5 @@ export function BtvRulesPage() {
         ></Viewer>
       </Worker>
     </Box>
-    // <Box>
-    //   <Box sx={{ backgroundColor: "#d0d0d0" }}>
-    //     <Typography variant="h1" textAlign="center">
-    //       Beyond the Veil: Basic Rules
-    //     </Typography>
-    //     <Typography textAlign="center">
-    //       In Beyond The Veil, the world has characters that each have access to
-    //       The Veil, a source of magical power. Most characters can only access a
-    //       small amount of power, with those rare few who can freely use the
-    //       magic ascending to effective god-hood. <B></B>In this world, it is not
-    //       just scholars and priests that utilise in magic; no successful
-    //       swordsman or archer is without their magical guidance or enhanced
-    //       weaponry. From defensive wards to elemental explosions, magic forms an
-    //       integral part in combat. <B></B>Some choose not to weaponise their
-    //       magic, however, with many - such as minstrels and chanters, using
-    //       their connection to The Veil to impress others, gaining their favour,
-    //       or in more sinister cases charm them out of their wealth.
-    //     </Typography>
-    //     <Typography variant="h2" textAlign="center">
-    //       Gameplay
-    //     </Typography>
-    //     <Typography textAlign="center">
-    //       The game is played by two types of players.
-    //       <br></br>
-    //       <B></B>• The Game Master (GM), whose job it is to narrate and act out
-    //       the environment, world, and people within.
-    //       <B></B>• The Player, who assumes the role of one character within the
-    //       world.
-    //       <br></br>
-    //       <br></br> The game is usually played with 1 GM and 3-4 Players.
-    //     </Typography>
-
-    //     <Typography variant="h2" textAlign="center">
-    //       Levels
-    //     </Typography>
-    //     <Typography textAlign="center">
-    //       To represent a character's power and experience, each character has a
-    //       Level. Most characters start at Level 1, and gain experience as they
-    //       defeat enemies or accomplish goals.<br></br> When a character gains a
-    //       level, they will gain a benefit based on their class.
-    //       <br></br> Levels range from 1 to 10, with later levels being slower to
-    //       increase from than earlier levels.
-    //     </Typography>
-
-    //     <Attributes></Attributes>
-    //     <HitPoints></HitPoints>
-    //     <DicePools></DicePools>
-    //     <Resting></Resting>
-    //   </Box>
-    // </Box>
   );
 }

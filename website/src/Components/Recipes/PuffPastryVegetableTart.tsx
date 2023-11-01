2b773f3528093a55a0e87b89cb18273d7afbf994
Joel <joelcox.joel@gmail.com>
2023-11-01 21:15:47 +0000

    puff pastry yum

--- website/src/Components/Recipes/PuffPastryVegetableTart.tsx
+++ website/src/Components/Recipes/PuffPastryVegetableTart.tsx
@@ -21,61 +21,37 @@ export function PuffPastryVegetableTart() {
         Turn on the oven to 200ºC/gas 6. Carefully cut the pastry block in half
         with a table knife. Wrap the other half and refrigerate or freeze for
         later.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Dust some flour onto a clean work surface and, using a rolling pin, roll
         out the pastry into a square, measuring 26cm x 26cm. Cut into 4 equal
         squares.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Place the pastry squares on a baking tray, leaving a space between each.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Using the back of a spoon, spread the centre of each square with pesto,
         but don't spread it onto the edges.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Squash the tomatoes into a large mixing bowl, then snap the asparagus
         spears into 3cm pieces. Keep the lovely pointy tips and a little of the
         stalk, but discard the end 3cm.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Using a speed peeler, carefully shred the courgettes into ribbons. Tear
         the roasted peppers into strips and add to the bowl.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Pick the basil leaves, reserving the pretty ones for later. Place the
         large ones in the mixing bowl.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Mix the vegetables together in the bowl, adding a splash of oil. Pile a
         little of the mixture on each pesto-smeared tart and top with two olives
-        .
-        <br>
-          <br></br>
-        </br>
+        .<br></br>
         Break up the mozzarella and place little bits on top of each tart - this
-        will make it gooey like a pizza. Grate over some Parmesan .
-        <br>
-          <br></br>
-        </br>
+        will make it gooey like a pizza. Grate over some Parmesan .<br></br>
         Bake for 15 to 20 minutes, until the pastry is golden and the cheese is
         all bubbly.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
         Once the tarts are ready, allow to cool slightly. Sprinkle with the
         reserved basil leaves and serve with a nice salad for lunch.
-        <br>
-          <br></br>
-        </br>
+        <br></br>
       </Typography>
     </Box>
   );
